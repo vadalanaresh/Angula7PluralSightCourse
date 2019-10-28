d2372b054a9c8d5383205a4183474d921fea1a1d
Naresh Vadala <naresh.vadala@questionmark.com>
2019-10-28 17:17:35 +0530

    Examples of ngfor and ngif

--- src/app/events/events.thumbnail.component.ts
+++ src/app/events/events.thumbnail.component.ts
@@ -4,21 +4,25 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     selector: "events-thumbnail",
     template: `
      <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{ event.price }}</div>
-        <div>
-            <span>Location: {{event.location.address}}</span>
-            <span class = "pad-left">{{event.location.city}}, {{event.location.country}}</span>
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div>Time: {{event?.time}}</div>
+        <div>Price: \${{ event?.price }}</div>
+        <div *ngIf="event?.location">
+            <span>Location: {{event?.location?.address}}</span>
+            <span class = "pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+        </div>
+        <div *ngIf="event?.onlineUrl">
+            <span>Online url:{{event?.onlineUrl}}</span>
         </div>
     </div>
     `,
     styles: [`
+    .thumbnail { min-height: 210px }
     .pad-left {margin-left:10px}
-    .well div{ color: #bbb }
+    .well div { color: #bbb }
     `]
 })
 export class EventsThumbnail {
-    @Input() event: any;
+    @Input() event?: any;
 }
