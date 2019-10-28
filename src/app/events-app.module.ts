714f81fac5cb34b69f7a8b550f9269264c769627
Naresh Vadala <naresh.vadala@questionmark.com>
2019-10-28 15:58:24 +0530

    Adding navbar to ui removing code related to template property

--- src/app/events-app.module.ts
+++ src/app/events-app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
-import { EventsThumbnail } from './events/events.thumbnail.component'
+import { EventsThumbnail } from './events/events.thumbnail.component';
+import { navbarComponent } from './nav/navbar.component';
+
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
-    EventsThumbnail
+    EventsThumbnail,
+    navbarComponent
   ],
   imports: [
     BrowserModule
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,17 +1,16 @@
-import { Component } from "@angular/core"
+import { Component } from "@angular/core";
+import { navbarComponent } from "../nav/navbar.component";
 
 @Component({
     selector: "events-list",
     template: `<div>
     <h1>Upcoming Angular Events</h1>
     <hr />
+    <nav-bar></nav-bar>
     <events-thumbnail #thumbnail [event] = "eventVariable"></events-thumbnail>
     </div>
-    <h3>{{thumbnail.someProperty}}</h3>
-    <button (click)="thumbnail.logFoo()"> Button </button>
     `
 })
-//template property is one of the way to pass data from child to parent it is simpler than input property
 export class EventsListComponent {
 
     eventVariable = {
